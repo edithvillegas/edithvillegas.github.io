5c73186412adb9d81730f48ea8d9f8c522ad94cb
edithvillegas <29732178+edithvillegas@users.noreply.github.com>
2026-05-11 14:35:28 +0000

    Deploying to gh-pages from @ edithvillegas/edithvillegas.github.io@21d7074bc2a8fff824f445ee4f19c90a192e4ab8 🚀

--- fr-ca/assets/js/search-data.js
+++ fr-ca/assets/js/search-data.js
@@ -41,13 +41,6 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/fr-ca/cv/";
           },
-        },{id: "nav-teaching",
-          title: "teaching",
-          description: "Materials for courses you taught. Replace this text with your description.",
-          section: "menu de navigation",
-          handler: () => {
-            window.location.href = "/fr-ca/teaching/";
-          },
         },{id: "post-a-post-with-plotly-js",
         
           title: "a post with plotly.js",
